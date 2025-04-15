86cfb18ca03b0272b6fce9bb93f46efddf3518ab
Joey Call <joeycall@Joeys-MacBook-Pro.local>
2025-04-15 10:05:29 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c80adf4c-d9d7-4079-90a7-aa511ed9666b';
+export const MODAL_CLOSE_SEC = 2.5;
